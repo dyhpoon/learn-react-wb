7ed08320305691923202b2fec73f8b4dcf41621b
Darren Poon <dyhpoon@gmail.com>
2018-12-05 15:54:18 +0800

    Add Me query

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,19 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
 const db = require('./db')
+const jwt = require('jsonwebtoken')
 
 const server = createServer()
 
-// TODO: use express middleware to handle cookies
 server.express.use(cookieParser())
-// TODO: use express middleware to populate current user
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    req.userId = userId
+  }
+  next()
+})
 
 server.start({
   cors: {
---
+++ frontend/components/User.js
@@ -0,0 +1,32 @@
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+
+const CURRENT_USER_QUERY = gql`
+  query {
+    me {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`
+
+export default class User extends Component {
+  render() {
+    return (
+      <Query {...this.props} query={CURRENT_USER_QUERY}>
+        {payload => this.props.children(payload)}
+      </Query>
+    )
+  }
+}
+
+User.propTypes = {
+  children: PropTypes.func.isRequired
+}
+
+export { CURRENT_USER_QUERY };
+
