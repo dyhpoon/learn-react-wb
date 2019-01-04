ede9173530984f20df4bc78be6d7a0a0935de094
Darren Poon <dyhpoon@gmail.com>
2019-01-04 13:26:06 +0800

    Add an enzyme shallow test

---
+++ frontend/__tests__/item.test.js
@@ -0,0 +1,28 @@
+import ItemComponent from '../components/Item';
+import { shallow } from 'enzyme'
+
+const fakeItem = {
+  id: 'abc123',
+  title: 'the title',
+  price: 3013,
+  description: 'this is a description',
+  image: 'dog.jpg',
+  largeImage: 'largedog.jpg',
+}
+
+describe('<Item/>', () => {
+  it('renders and displays properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem}/>)
+    expect(wrapper.find('PriceTag').children().text()).toBe('$30.13')
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title)
+    expect(wrapper.find('img').props().src).toBe(fakeItem.image)
+    expect(wrapper.find('img').props().alt).toBe(fakeItem.title)
+  })
+
+  it('renders out the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem}/>)
+    const buttonList = wrapper.find('.buttonList');
+    expect(buttonList.children()).toHaveLength(3)
+    expect(buttonList.find('Link').exists()).toBeTruthy()
+  })
+})
